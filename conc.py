b7745fefd0f6b504cb00724700b41192e4981496
Ricardo  Coelho  Ferreira <spout2009@hotmail.com>
2015-05-02 01:17:06 -0300

    Update conc.py

--- conc.py
+++ conc.py
@@ -1,4 +1,135 @@
 # -*- coding: iso-8859-15 -*-
+
+def corrige(nome):
+    with open (nome,'r') as file:
+        e=[]
+        with open('corrigido.tex','w') as  stream:
+            s=[]
+            arquiv=file.read()
+            for r in arquiv:
+                if r=='_':
+                    s.append('\\')
+                    s.append('_')
+                else:
+                    s.append(r)
+            s=''.join(s)
+            stream.write(s)
+
+
+def analisa(nome):
+    with open(nome,'r') as file:
+        instancia=[]
+        sol=[]
+        tempo=[]
+        alg121=[]
+        alg131=[]
+        alg221=[]
+        alg231=[]
+        alg321=[]
+        alg331=[]
+        mbc221=[]
+        mbc231=[]
+        s=file.readlines()
+        print(len(s))
+        r=range(1,len(s)/2+1)
+        for i in r:
+            del s[i]
+        for k in range(len(s)):
+            s[k]=s[k].split('&')
+            
+        for i in range(len(s)):#para  arrancar o '\\ ' do final
+            s[i][len(s[i])-1]=list( s[i][len(s[i])-1])
+            del s[i][len(s[i])-1][len(s[i][len(s[i])-1])-1]
+            del s[i][len(s[i])-1][len(s[i][len(s[i])-1])-1]
+            del s[i][len(s[i])-1][len(s[i][len(s[i])-1])-1]
+            s[i][len(s[i])-1]=''.join(s[i][len(s[i])-1])
+            
+        alg131=s[0:30]
+        alg121=s[30:60]
+        alg231=s[60:90]
+        alg221=s[90:120]
+        alg331=s[120:150]
+        alg321=s[150:180]
+        mbc231=s[180:210]
+        mbc221=s[210:240]
+        for s in range(len(mbc231)):
+            del mbc231[s][3]
+            del mbc231[s][3]
+            del mbc221[s][3]
+            del mbc221[s][3]
+        analise2(alg121,mbc221,'alg121.tex')
+        analise2(alg131,mbc231,'alg131.tex.')
+        analise2(alg221,mbc221,'alg221.tex')
+        analise2(alg231,mbc231,'alg231.tex')
+        analise2(alg321,mbc221,'alg321.tex')
+        analise2(alg331,mbc231,'alg331.tex')
+        
+def analise2(novo,mbc,nome):
+    em=mbc[20:30]
+    cm=mbc[0:10]
+    rm=mbc[10:20]
+    enovo=novo[20:30]
+    rnovo=novo[10:20]
+    cnovo=novo[0:10]
+    gap1=[]
+    gap2=[]
+    gap3=[]
+    for k in  range(len(em)):
+        em[k][1]=float(em[k][1])
+        em[k][2]=float(em[k][2])
+        cm[k][1]=float(cm[k][1])
+        cm[k][2]=float(cm[k][2])
+        rm[k][1]=float(rm[k][1])
+        rm[k][2]=float(rm[k][2])
+        enovo[k][1]=float(enovo[k][1])
+        enovo[k][2]=float(enovo[k][2])
+        rnovo[k][1]=float(rnovo[k][1])
+        rnovo[k][2]=float(rnovo[k][2])
+        cnovo[k][1]=float(cnovo[k][1])
+        cnovo[k][2]=float(cnovo[k][2])
+
+    for k in range(10):
+        try: 
+            gap1.append((cnovo[k][1]-cm[k][1])/cnovo[k][1])
+        except ZeroDivisionError:
+            gap1.append(0)
+        try:
+            gap2.append((rnovo[k][1]-rm[k][1])/rnovo[k][1])
+        except ZeroDivisionError:
+            gap2.append(0)
+        try:
+            gap3.append((enovo[k][1]-em[k][1])/enovo[k][1])
+        except ZeroDivisionError:
+            gap3.append(0)
+    with open (nome,'w') as file:
+        file.write('\\begin{tabular}{|c|c|c|c|c|}'+'\n \\hline \\multicolumn{5}{|c|}')
+        file.write('{\\textbf{Resultados do Relax And Fix (instâncias c)}}\\\\ \n')
+        file.write('\\hline \n Instância &Solução MBC2 & Solução RF & Tempo RF & GAP')
+        file.write('\\\\ \n \\hline \\hline \n')
+        for  i in range(10):
+            file.write(cnovo[i][0]+'&'+str(cm[i][1])+' & '+str(cnovo[i][1])+' & ')
+            file.write(str(cnovo[i][2])+' & '+str(gap1[i])+'\\\\ \n \\hline \n')   
+        file.write('\\end{tabular}'+'\n')    
+        file.write('\\begin{tabular}{|c|c|c|c|c|}'+'\n \\hline \\multicolumn{5}{|c|}')
+        file.write('{\\textbf{Resultados do Relax And Fix (instâncias r)}}\\\\ \n')
+        file.write('\\hline \n Instância &Solução MBC2 & Solução RF & Tempo RF & GAP')
+        file.write('\\\\ \n \\hline \\hline \n')
+        for  i in range(10):
+            file.write(rnovo[i][0]+' & '+str(rm[i][1])+' & '+str(rnovo[i][1])+' & ')
+            file.write(str(rnovo[i][2])+' & '+str(gap2[i])+'\\\\ \n \\hline \n')
+        file.write('\\end{tabular}'+'\n')
+        file.write('\\begin{tabular}{|c|c|c|c|c|}'+'\n \\hline \\multicolumn{5}{|c|}')
+        file.write('{\\textbf{Resultados do Relax And Fix (instâncias e)}}\\\\ \n')
+        file.write('\\hline \n Instância &Solução MBC2 & Solução RF & Tempo RF & GAP')
+        file.write('\\\\ \n \\hline \\hline \n')
+        for  i in range(10):
+            file.write(enovo[i][0]+' & '+str(em[i][1])+' & '+str(enovo[i][1])+' & ')
+            file.write(str(enovo[i][2])+' & '+str(gap3[i])+'\\\\ \n \\hline \n')
+        file.write('\\end{tabular}'+' \n')  
+            
+            
+    
+    
 def floatCastVet(vet):
     for r in range(0,len(vet)):
         vet[r]=float(vet[r])
@@ -87,3 +218,5 @@ v.append(floatCastVet(u[1]))
 print(e)
 print(v)
 gapAndStat('tabela.tex',v,e,u[2],'MBC alterado solução inteira para 21 nós(instâncias c)')
+
+
